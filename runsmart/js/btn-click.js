7336aca744397d90107fefd05cd0a46c87a9927c
Konstantin <konstantin.karzhanov@gmail.com>
2022-08-02 13:04:26 -0230

    added changable chosenProductName in modal

--- runsmart/js/btn-click.js
+++ runsmart/js/btn-click.js
@@ -4,18 +4,24 @@ const modals = document.querySelector(".modals");
 const callRequestForm = modals.querySelector(".modals__call-request");
 const orderForm = modals.querySelector(".modals__order");
 
-btn_main.forEach((item) => {
-    item.addEventListener("click", () => {
-        item.style.transform = "scale(1.03)";
-        setTimeout(() => item.style.transform = null, 150);
+btn_main.forEach((button) => {
+    button.addEventListener("click", () => {
+        button.style.transform = "scale(1.03)";
+        setTimeout(() => button.style.transform = null, 150);
         
-        if(item.classList.contains("call-request-btn")){
+        if(button.classList.contains("call-request-btn")){
+
             callRequestForm.showModal();
 
             closeModal(callRequestForm);
 
-        } else if(item.classList.contains("buy-btn")){
+        } else if(button.classList.contains("buy-btn")){
+            const currentButtonContainer = button.parentNode;
+            const catalogItem = currentButtonContainer.parentNode;
+            const chosenProductName = catalogItem.querySelector("h3").textContent;
+            
             orderForm.showModal();
+            orderForm.querySelector("span").textContent = chosenProductName;
 
             closeModal(orderForm);
         } 
