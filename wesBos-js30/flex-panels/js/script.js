4232deb27b2761bd8221e12b56b18faff820ba7a
Konstantin <konstantin.karzhanov@gmail.com>
2022-09-09 09:55:27 -0230

    didn't work on adaptivity

---
+++ wesBos-js30/flex-panels/js/script.js
@@ -0,0 +1,24 @@
+const panelContainer = document.querySelector(".panel-container");
+
+panelContainer.addEventListener("click", event => {
+	const targetPanel = event.target.closest(".panel-container__item");
+
+	if(!targetPanel) return;
+
+	if(!targetPanel.hasAttribute("data-show-closer")){
+		targetPanel.dataset.showCloser = "";
+		targetPanel.addEventListener("transitionend", showCaption);
+	} else {
+		targetPanel.removeEventListener("transitionend", showCaption);
+
+		delete targetPanel.dataset.showCloser;
+		delete targetPanel.dataset.showCaption;
+	}
+
+})
+
+function showCaption(event){
+	if(event.propertyName.includes("flex")){
+		this.dataset.showCaption = "";
+	}
+}
