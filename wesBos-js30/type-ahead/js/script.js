0ea3fde7b5858ce8896102234e220552633c6849
Konstantin <konstantin.karzhanov@gmail.com>
2022-09-15 12:24:30 -0230

    commit without comment

--- wesBos-js30/type-ahead/js/script.js
+++ wesBos-js30/type-ahead/js/script.js
@@ -18,8 +18,9 @@ function displayMatches(){
 	const placesMatched = findMatches(this.value, places);
 	const html = placesMatched.map(place => {
 		const regex = new RegExp(this.value, "gi");
-		const cityName = place.city.replace(regex, `<span class="city-hl">${this.value}</span>`)
-		const stateName = place.state.replace(regex, `<span class="state-hl">${this.value}</span>`) 
+		const cityName = place.city.replace(regex, `<span class="city-hl">${this.value}</span>`);
+		const stateName = place.state.replace(regex, `<span class="state-hl">${this.value}</span>`); 
+
 		return `<li class="search__suggestions-item"><span>${cityName}, ${stateName}</span><span class="population">${numberWithCommas(place.population)}</span></li>`
 	}).join("");
 
