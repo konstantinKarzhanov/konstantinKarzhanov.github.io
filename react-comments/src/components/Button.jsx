01d215ed6e6aeb405c7803df79e4ebee03da51ef
kostiantyn <kostiantyn@test.com>
2023-07-30 01:42:27 -0230

    removed console.log from the Button

--- react-comments/src/components/Button.jsx
+++ react-comments/src/components/Button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Button = ({ idHandle, typeHandle, disabledHandle, children }) => {
-  console.log(disabledHandle);
   return (
     <button id={idHandle} type={typeHandle} disabled={disabledHandle}>
       {children}
