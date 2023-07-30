e316da38df98c10bafed28040c5d319807af4c79
kostiantyn <kostiantyn@test.com>
2023-07-29 22:38:35 -0230

    +FeedbackStatisticSection

--- react-comments/src/App.js
+++ react-comments/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { ContextProvider } from "./components/Context";
 import FeedbackSection from "./components/FeedbackSection";
+import FeedbackStatisticSection from "./components/FeedbackStatisticSection";
 import FeedbackListSection from "./components/FeedbackListSection";
 
 const App = () => {
   return (
-    // btnDisabled
+    // btnDisabled --> how to do validation before Submit and not inside it?
+    // need to check the length statically for submitted elements
     // textarea doesn't change its size after submitting
     <>
       <header></header>
@@ -13,6 +15,7 @@ const App = () => {
         <ContextProvider>
           <h1>Rate us</h1>
           <FeedbackSection />
+          <FeedbackStatisticSection />
           <FeedbackListSection />
         </ContextProvider>
       </main>
---
+++ react-comments/src/components/FeedbackStatisticSection.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import Context from "./Context";
+
+const FeedbackStatisticSection = () => {
+  const { feedbackArr } = useContext(Context);
+  const averageRating = feedbackArr.reduce((acc, { rating }, index) => {
+    acc += rating;
+
+    if (index === feedbackArr.length - 1)
+      return +(acc / feedbackArr.length).toFixed(1);
+    return acc;
+  }, 0);
+
+  return (
+    <section>
+      <div>
+        <p>
+          Feedbacks #: <span>{feedbackArr.length}</span>
+        </p>
+        <p>
+          Average Rating: <span>{averageRating}</span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default FeedbackStatisticSection;
